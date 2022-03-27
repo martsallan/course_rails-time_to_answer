b36674c5d9d47ecf1dcd5e27167077bfcdcedc35
Allan Martins <allanmartins@id.uff.br>
2022-03-27 07:45:48 -0300

    create search form for questions

---
+++ app/controllers/site/search_controller.rb
@@ -0,0 +1,10 @@
+class Site::SearchController < SiteController
+  def questions
+    #params[:term]
+    #@questions = Question.includes(:answers).page(params[:page])
+    @questions = Question.includes(:answers)
+                         .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
+                         .page(params[:page])
+
+  end
+end
