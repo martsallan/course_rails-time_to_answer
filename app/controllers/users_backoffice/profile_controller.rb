ee4e76f411c7d9be3db62ea98f5bacc917fbaa61
Allan Martins <allanmartins@id.uff.br>
2022-03-29 12:10:26 -0300

    add extra data to user

--- app/controllers/users_backoffice/profile_controller.rb
+++ app/controllers/users_backoffice/profile_controller.rb
@@ -3,12 +3,12 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
   before_action :set_user
 
   def edit
-
+    @user.build_user_profile if @user.user_profile.blank?
   end
 
   def update
     if @user.update(params_user)
-      sign_in(@user, bypass:true)
+      bypass_sign_in(@user)
       redirect_to users_backoffice_profile_path, notice: "Usuário atualizado com sucesso!"
     else
       render :edit
@@ -22,7 +22,8 @@ class UsersBackoffice::ProfileController < UsersBackofficeController
   end
 
   def params_user
-    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation,
+    user_profile_attributes: [:address, :gender, :birthday])
   end
 
   def verify_password
